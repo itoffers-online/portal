7565c01adfd72601d1f24a601dc161b63dfe5bad
ZielinskiLukasz <lucas.zielinski@gmail.com>
2019-02-13 16:49:09 +0100

    Fixed a typo in 'Facbook' namespace

--- php/hireinsocial/src/HireInSocial/Infrastructure/Facebook/FacebookGraphSDK.php
+++ php/hireinsocial/src/HireInSocial/Infrastructure/Facebook/FacebookGraphSDK.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace HireInSocial\Infrastructure\Facbook;
+namespace HireInSocial\Infrastructure\Facebook;
 
 use Facebook\Exceptions\FacebookSDKException;
 use Facebook\FacebookResponse;
