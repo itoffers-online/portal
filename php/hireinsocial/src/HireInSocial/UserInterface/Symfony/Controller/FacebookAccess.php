28d0fbb793b783f86c51b13ecdb7dbfa4052d9fb
Norbert Orzechowicz <norbert@orzechowicz.pl>
2019-02-13 20:52:33 +0100

    Store Facebook access token and use it to obtain Facebook application user id (#22)

---
+++ php/hireinsocial/src/HireInSocial/UserInterface/Symfony/Controller/FacebookAccess.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HireInSocial\UserInterface\Symfony\Controller;
+
+use Facebook\Authentication\AccessToken;
+use Facebook\Facebook;
+use Psr\Log\LoggerInterface;
+
+trait FacebookAccess
+{
+    public function getUserId(Facebook $facebook, AccessToken $accessToken, LoggerInterface $logger) : string
+    {
+        $facebookResponse = $facebook->get('me', $accessToken);
+
+        $logger->debug('Facebook /me response', ['body' => $facebookResponse->getBody()]);
+
+        return $facebookResponse->getDecodedBody()['id'];
+    }
+}
