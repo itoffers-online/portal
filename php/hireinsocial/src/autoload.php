c1d9405bac365ffdbc7e01d92f1bb6ee03a732b5
Norbert Orzechowicz <norbert@orzechowicz.pl>
2019-02-27 23:09:33 +0100

    Added file headers

--- php/hireinsocial/src/autoload.php
+++ php/hireinsocial/src/autoload.php
@@ -1,3 +1,12 @@
 <?php
 
+/*
+ * This file is part of the Hire in Social project.
+ *
+ * (c) Norbert Orzechowicz <norbert@orzechowicz.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require dirname(__DIR__) . '/vendor/autoload.php';
--- php/hireinsocial/tests/bootstrap.php
+++ php/hireinsocial/tests/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Hire in Social project.
+ *
+ * (c) Norbert Orzechowicz <norbert@orzechowicz.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require dirname(__DIR__) . '/src/autoload.php';
 
 define('ROOT_DIR', dirname(__DIR__));
